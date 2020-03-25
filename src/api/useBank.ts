dcde0fa72a5bf3e783f8c87ded5ed199292115da
sim <sim@terra.money>
2020-03-25 14:09:36 +0900

    fix(Assets): Show 0 if available amount is less than 0

--- src/api/useBank.ts
+++ src/api/useBank.ts
@@ -1,7 +1,21 @@
-import { BankAPI, BankData, User } from '../types'
+import { BankAPI, BankData, User, Balance } from '../types'
+import { lt } from '../utils'
 import useFCD from './useFCD'
 
 export default ({ address }: User): BankAPI => {
-  const response = useFCD<BankData>({ url: `/v1/bank/${address}` })
-  return response
+  const { data, ...rest } = useFCD<BankData>({ url: `/v1/bank/${address}` })
+
+  const fixAvailable = (balance: Balance[]): Balance[] =>
+    balance.map(({ available, ...rest }) => ({
+      ...rest,
+      available: lt(available, 0) ? '0' : available
+    }))
+
+  return Object.assign(
+    {},
+    rest,
+    data && {
+      data: Object.assign({}, data, { balance: fixAvailable(data.balance) })
+    }
+  )
 }
