9c94f4e9c9cf15c93ae8b8700cbc8b00b8f7cb22
sim <sim@terra.money>
2020-12-24 17:46:18 +0900

    feature: Add bsc shuttle address

--- src/post/useSend.ts
+++ src/post/useSend.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { Dictionary, last } from 'ramda'
+import { AccAddress } from '@terra-money/terra.js'
 import { ethers } from 'ethers'
 import { BankData, TxsData, Tx, RecentSentUI, RecentSentItemUI } from '../types'
 import { PostPage, Coin, User, Field } from '../types'
@@ -18,15 +19,30 @@ import { isAvailable, getFeeDenomList, isFeeAvailable } from './validateConfirm'
 import { calc } from './txHelpers'
 import useFetchTax from './useFetchTax'
 
-const SHUTTLE: Dictionary<string> = {
-  mainnet: 'terra13yxhrk08qvdf5zdc9ss5mwsg5sf7zva9xrgwgc',
-  testnet: 'terra10a29fyas9768pw8mewdrar3kzr07jz8f3n73t3'
+enum RecipientNetwork {
+  Terra = 'Terra',
+  Ethereum = 'Ethereum',
+  BSC = 'Binance Smart Chain'
+}
+
+const SHUTTLES: Dictionary<Record<RecipientNetwork, string>> = {
+  mainnet: {
+    [RecipientNetwork.Terra]: '',
+    [RecipientNetwork.Ethereum]: 'terra13yxhrk08qvdf5zdc9ss5mwsg5sf7zva9xrgwgc',
+    [RecipientNetwork.BSC]: 'terra1g6llg3zed35nd3mh9zx6n64tfw3z67w2c48tn2'
+  },
+  testnet: {
+    [RecipientNetwork.Terra]: '',
+    [RecipientNetwork.Ethereum]: 'terra10a29fyas9768pw8mewdrar3kzr07jz8f3n73t3',
+    [RecipientNetwork.BSC]: 'terra1paav7jul3dzwzv78j0k59glmevttnkfgmgzv2r'
+  }
 }
 
 interface Values {
   to: string
   input: string
   memo: string
+  network: RecipientNetwork
 }
 
 export default (user: User, denom: string): PostPage<RecentSentUI> => {
@@ -36,38 +52,46 @@ export default (user: User, denom: string): PostPage<RecentSentUI> => {
   const { list: tokens, loading: tokenLoading, whitelist } = tokenBalance
   const loading = bankLoading || tokenLoading
   const { chain } = useConfig()
-  const shuttle = SHUTTLE[chain.current.name]
   const v = validateForm(t)
 
   /* recent txs */
   const url = '/v1/msgs'
   const params = { account: user.address, action: 'send', page: 1 }
   const txsResponse = useFCD<TxsData>({ url, params })
+
+  const renderRecentItem = ({
+    date,
+    values
+  }: RecentSentItem): RecentSentItemUI => {
+    const { to, input, memo: $memo } = values
+
+    const toEthereum = Object.values(shuttles).includes(to)
+    const network = toEthereum
+      ? (Object.entries(shuttles).find(
+          ([, address]) => address === to
+        )![0] as RecipientNetwork)
+      : RecipientNetwork.Terra
+
+    const recipient = !toEthereum ? to : $memo
+    const memo = !toEthereum ? $memo : ''
+
+    return {
+      title: date,
+      contents: [
+        { title: t('Post:Send:Send to'), content: recipient },
+        { title: t('Post:Send:Network'), content: network },
+        { title: t('Common:Tx:Amount'), content: `${input} ${unit}` },
+        { title: t('Common:Tx:Memo'), content: memo }
+      ].filter(({ content }) => content),
+      onClick: () => form.setValues({ ...values, to: recipient, network, memo })
+    }
+  }
+
   const renderRecent = ({
     totalCnt,
     txs
   }: TxsData): RecentSentUI | undefined => {
     const recent = !gt(totalCnt, 0) ? undefined : findRecent(txs, denom)
-
-    const renderRecentItem = ({
-      date,
-      values
-    }: RecentSentItem): RecentSentItemUI => {
-      const { to, input, memo: $memo } = values
-      const toEthereum = shuttle === to
-      const recipient = !toEthereum ? to : $memo
-      const memo = !toEthereum ? $memo : ''
-
-      return {
-        title: date,
-        contents: [
-          { title: t('Post:Send:Send to'), content: recipient },
-          { title: t('Common:Tx:Amount'), content: `${input} ${unit}` },
-          { title: t('Common:Tx:Memo'), content: memo }
-        ].filter(({ content }) => content),
-        onClick: () => form.setValues({ ...values, to: recipient, memo })
-      }
-    }
 
     return !recent?.length
       ? undefined
@@ -116,25 +140,64 @@ export default (user: User, denom: string): PostPage<RecentSentUI> => {
   }, [loading])
 
   /* form */
-  const validate = ({ input, to, memo }: Values) => ({
+  const validate = ({ input, to, memo, network }: Values) => ({
     to: v.address(to, true),
     input: v.input(input, { max: toInput(max.amount) }),
     memo:
       v.length(memo, { max: 256, label: t('Common:Tx:Memo') }) ||
       v.includes(memo, '<') ||
-      v.includes(memo, '>')
+      v.includes(memo, '>'),
+    network: !network
+      ? t('Common:Validate:{{label}} is required', {
+          label: t('Post:Send:Network')
+        })
+      : ''
   })
 
-  const initial = { to: '', input: '', memo: '' }
+  const initial = {
+    to: '',
+    input: '',
+    memo: '',
+    network: RecipientNetwork.Terra
+  }
+
   const form = useForm<Values>(initial, validate)
-  const { values, setValue, invalid, getDefaultProps, getDefaultAttrs } = form
-  const { to, input, memo: $memo } = values
+  const { values, setValue, setValues, invalid } = form
+  const { getDefaultProps, getDefaultAttrs } = form
+  const { to, input, memo: $memo, network } = values
   const amount = toAmount(input)
   const toEthereum = ethers.utils.isAddress(to)
+  const toTerra = AccAddress.validate(to)
+  const shuttles = SHUTTLES[chain.current.name]
+  const shuttle = shuttles[network]
+
+  useEffect(() => {
+    toEthereum &&
+      network === RecipientNetwork.Terra &&
+      setValues(values => ({ ...values, network: RecipientNetwork.Ethereum }))
+
+    toTerra &&
+      network !== RecipientNetwork.Terra &&
+      setValues(values => ({ ...values, network: RecipientNetwork.Terra }))
+  }, [toTerra, toEthereum, network, setValues])
 
   /* render */
   const unit = format.denom(denom)
   const fields: Field[] = [
+    {
+      ...getDefaultProps('network'),
+      element: 'select',
+      label: t('Post:Send:Network'),
+      attrs: { ...getDefaultAttrs('network') },
+      options: Object.values(RecipientNetwork).map(value => {
+        const disabled = {
+          [RecipientNetwork.Terra]: toEthereum,
+          [RecipientNetwork.Ethereum]: toTerra,
+          [RecipientNetwork.BSC]: toTerra
+        }[value]
+        return { value, children: value, disabled }
+      })
+    },
     {
       ...getDefaultProps('to'),
       label: t('Post:Send:Send to'),
@@ -246,14 +309,11 @@ export default (user: User, denom: string): PostPage<RecentSentUI> => {
 }
 
 /* helper */
-type RecentSentItem = { date: string; values: Values }
+type RecentSentItem = { date: string; values: Omit<Values, 'network'> }
 const findRecent = (txs: Tx[], denom: string): RecentSentItem[] | undefined => {
   try {
-    const reduced = txs.reduce(
-      (
-        acc: { [key: string]: RecentSentItem },
-        { msgs: [{ text, out }], memo, timestamp }
-      ) => {
+    const reduced = txs.reduce<Dictionary<RecentSentItem>>(
+      (acc, { msgs: [{ text, out }], memo, timestamp }) => {
         const to = last(text.split(' to '))
         const coin = out![0]
 
