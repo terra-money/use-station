1f9e7bd2c26d48587f3490c25dc188637592c06b
blue <blue@terra.money>
2021-03-26 15:36:28 +0900

    fix memory leak warning in useFCD

--- src/api/useFCD.ts
+++ src/api/useFCD.ts
@@ -1,5 +1,8 @@
-import { useState, useEffect, useCallback } from 'react'
-import axios, { AxiosError, AxiosRequestConfig as Config } from 'axios'
+import { useState, useEffect, useCallback, useRef } from 'react'
+import axios, {
+  AxiosError,
+  AxiosRequestConfig as Config,
+} from 'axios'
 import qs from 'qs'
 import { API } from '../types'
 import fcd from './fcd'
@@ -9,6 +12,7 @@ export default <T>(
   { url, params }: Config,
   immediate: boolean = true
 ): API<T> => {
+  const mounted = useRef(false)
   const [data, setData] = useState<T>()
   const [loading, setLoading] = useState<boolean>(immediate)
   const [error, setError] = useState<Error | AxiosError>()
@@ -22,20 +26,26 @@ export default <T>(
       setError(undefined)
 
       const { token: cancelToken } = source
-      const { data } = await fcd.get<T>(`${url}?${query}`, { cancelToken })
+      const { data } = await fcd.get<T>(`${url}?${query}`, {
+        cancelToken,
+      })
       setData(data)
     } catch (error) {
       !axios.isCancel(error) && setError(error)
     } finally {
-      setLoading(false)
+      mounted.current && setLoading(false)
     }
 
     // eslint-disable-next-line
   }, [url, query])
 
   useEffect(() => {
+    mounted.current = true
     immediate && execute()
-    return () => source.cancel()
+    return () => {
+      mounted.current = false
+      source.cancel()
+    }
     // eslint-disable-next-line
   }, [execute, immediate])
 
