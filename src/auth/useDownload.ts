7f4f3ef1df4072dd9dd292bbace1583985ea0a88
Zeid Marouf <4321056+zmarouf@users.noreply.github.com>
2021-07-21 10:46:29 +0900

    chore(download): replace station download links with Github releases
    - Following website ee7066d

--- src/auth/useDownload.ts
+++ src/auth/useDownload.ts
@@ -10,14 +10,14 @@ export default (): Download => {
       {
         key: 'mac',
         label: 'Mac',
-        link: 'https://www.terra.dev/station/Terra%20Station-1.1.0.dmg',
+        link: 'https://github.com/terra-money/station/releases/download/v3.5.0/Terra.Station-1.1.0.dmg',
         ext: 'dmg',
       },
       {
         key: 'win',
         label: 'Windows',
         link:
-          'https://www.terra.dev/station/Terra%20Station%20Setup%201.1.0.exe',
+          'https://github.com/terra-money/station/releases/download/v3.5.0/Terra.Station.Setup.1.1.0.exe',
         ext: 'exe',
       },
       {
@@ -26,12 +26,12 @@ export default (): Download => {
         links: [
           {
             link:
-              'https://terra.money/station/station-electron_1.1.1_amd64.deb',
+              'https://github.com/terra-money/station/releases/download/v3.5.0/station-electron_1.1.1_amd64.deb',
             ext: 'deb',
           },
           {
             link:
-              'https://terra.money/station/station-electron-1.1.1.x86_64.rpm',
+              'https://github.com/terra-money/station/releases/download/v3.5.0/station-electron-1.1.1.x86_64.rpm',
             ext: 'rpm',
           },
         ],
